1cbd4764a12191b552bcc461e5ce07ada317b42c
Saakshi Adiga <f20201930@goa.bits-pilani.ac.in>
2023-06-12 10:55:57 +0530

    youtube api

---
+++ lib/youtube api/ytHappy.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_api/youtube_api.dart';
+
+class Happy extends StatefulWidget {
+  const Happy({Key? key}) : super(key: key);
+
+  @override
+  State<Happy> createState() => _Happy();
+}
+
+class _Happy extends State<Happy> {
+  static String key = "AIzaSyCS7SIK0IAmYdsJ4sE4mnj2xiJrghDLp_U";
+
+  YoutubeAPI youtube = YoutubeAPI(key);
+  List<YouTubeVideo> videoResult = [];
+
+  Future<void> callAPI() async {
+    String query = "Happy Songs";
+    videoResult = await youtube.search(
+      query,
+      order: 'relevance',
+      videoDuration: 'any',
+    );
+    videoResult = await youtube.nextPage();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    callAPI();
+    print('hello');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 47, 1, 70),
+        title: Text('Emotify'),
+    ),
+      body: ListView(
+        children: videoResult.map<Widget>(listItem).toList(),
+      ),
+    );
+  }
+
+  Widget listItem(YouTubeVideo video) {
+    return Card(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 7.0),
+        padding: EdgeInsets.all(12.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: Image.network(
+                video.thumbnail.small.url ?? '',
+                width: 120.0,
+              ),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    video.title,
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 3.0),
+                    child: Text(
+                      video.channelTitle,
+                      softWrap: true,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(
+                    video.url,
+                    softWrap: true,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
